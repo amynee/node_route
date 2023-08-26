a64c3c24f43941665be3357b5c4f066f5471f81c
amynee <tlijaniamine9@gmail.com>
2023-08-26 15:38:38 +0200

    adjust article controller

--- backend/routes/article.routes.js
+++ backend/routes/article.routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const { setArticle, getArticles } = require('../controllers/article.controller');
+const articleController = require('../controllers/article.controller');
 
 const router = express.Router();
 
 // Create the GET endpoint
-router.get('/', getArticles);
+router.get('/', articleController.getArticles);
 
 // Create the POST endpoint
-router.post('/', setArticle);
+router.post('/', articleController.setArticle);
 
 module.exports = router;
